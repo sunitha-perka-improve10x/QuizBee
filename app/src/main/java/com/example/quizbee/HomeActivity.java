ba630ae8046c48013c8411eb8a11be249902f808
sunitha <sunithapraveenkumar@gmail.com>
2023-06-07 12:50:40 +0530

    HomeActivity is working

--- app/src/main/java/com/example/quizbee/HomeActivity.java
+++ app/src/main/java/com/example/quizbee/HomeActivity.java
@@ -2,13 +2,28 @@ package com.example.quizbee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.quizbee.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+        buttonClick();
+    }
+
+    public void buttonClick(){
+        binding.startBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this,QuestionActivity.class);
+            startActivity(intent);
+        });
     }
 }
